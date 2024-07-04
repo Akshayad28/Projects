68229d5b8bb3ddc31a1c96e3c8f8e6cff7947357
Akshayad28 <akshayndeshmukh21@gmail.com>
2024-07-05 00:35:38 +0530

    changed code

--- src/main/java/com/example/AppTest.java
+++ src/main/java/com/example/AppTest.java
@@ -1,7 +1,6 @@
 package com.example;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
